9a663c619e2079651dd9537911b59475bfc3d15d
jaehyuneyea <jaehyune.yea@gmail.com>
2022-03-16 15:32:26 -0700

    Added reset button

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 let gridSize = 10;
 const MAX_WIDTH = 640;
-const container = document.querySelector('.container');
+const container = document.querySelector('.main-grid');
 container.style.height = `${MAX_WIDTH}px`;
 container.style.width = `${MAX_WIDTH}px`;
 
-for(let i=0; i < gridSize; i++) {
-  let row = document.createElement('div');
-  row.id = "row" + i;
-
-  container.appendChild(row);
-  let currRow = document.getElementById('row' + i);
-
-  for(let j=0; j < gridSize; j++) {
-    let box = document.createElement('div');
-    box.id = 'col' + j;
-    box.className = 'grid';
-    box.style.height = `${MAX_WIDTH / gridSize}px`;
-    box.style.width = `${MAX_WIDTH / gridSize}px`;
-    box.style.border = '1px solid black';
-    currRow.append(box);
+function createGrid(size) {
+  for(let i=0; i < size; i++) {
+    let row = document.createElement('div');
+    row.id = "row" + i;
+  
+    container.appendChild(row);
+    let currRow = document.getElementById('row' + i);
+  
+    for(let j=0; j < size; j++) {
+      let box = document.createElement('div');
+      box.id = 'col' + j;
+      box.className = 'grid';
+      box.style.height = `${MAX_WIDTH / size}px`;
+      box.style.width = `${MAX_WIDTH / size}px`;
+      box.style.border = '1px solid black';
+      currRow.append(box);
+    }
   }
 }
+
+createGrid(gridSize);
 
 let grids = document.querySelectorAll('.grid');
 Array.from(grids).forEach(grid => grid.addEventListener('mouseover', () => {
   grid.classList.add('coloured');
 }));
+
